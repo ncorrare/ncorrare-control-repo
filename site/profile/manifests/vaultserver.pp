73c1fb64f461896fb6a3aa6b1b410479e2906a9f
Nicolas Corrarello <nicolas@corrarello.com>
2017-08-06 19:26:08 +0100

    Missing class declaration

--- site/profile/manifests/vaultserver.pp
+++ site/profile/manifests/vaultserver.pp
@@ -1,15 +1,17 @@
-class { '::vault':
-  backend => {
-    consul => {
-      path    => 'vault',
-      address => '127.0.0.1:8500',
-    }
-  },
-  listener => {
-    tcp => {
-      address     => '127.0.0.1:8200',
-      tls_disable => 1,
-    }
-  },
-  disable_mlock => true,
+class profile::vaultserver {
+  class { '::vault':
+    backend => {
+      consul => {
+        path    => 'vault',
+        address => '127.0.0.1:8500',
+      }
+    },
+    listener => {
+      tcp => {
+        address     => '127.0.0.1:8200',
+        tls_disable => 1,
+      }
+    },
+    disable_mlock => true,
+  }
 }
