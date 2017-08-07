457176881bff377875eb004c33e41cfaede2e0a9
Nicolas Corrarello <nicolas@corrarello.com>
2017-08-07 11:10:01 +0100

    Changing fact due to old facter version

--- site/profile/manifests/consulserver.pp
+++ site/profile/manifests/consulserver.pp
@@ -5,7 +5,7 @@ class profile::consulserver {
       'bootstrap_expect' => 3,
       'client_addr'      => '0.0.0.0',
       'data_dir'         => '/opt/consul',
-      'bind_addr'        => $facts['networking']['interfaces']['eth0']['ip'],
+      'bind_addr'        => $facts['ipaddress_eth0'],
       'retry_join'       => ['172.16.10.57', '172.16.10.58', '172.16.10.54'],
       'datacenter'       => 'stn',
       'log_level'        => 'INFO',
