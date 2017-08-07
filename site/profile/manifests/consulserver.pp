232374e81f76625c4fc341c99afa33d9d5c914ed
Nicolas Corrarello <nicolas@corrarello.com>
2017-08-07 11:02:52 +0100

    Added retry join

--- site/profile/manifests/consulserver.pp
+++ site/profile/manifests/consulserver.pp
@@ -5,6 +5,7 @@ class profile::consulserver {
       'bootstrap_expect' => 3,
       'client_addr'      => '0.0.0.0',
       'data_dir'         => '/opt/consul',
+      'retry_join'       => ['172.16.10.57', '172.16.10.58', '172.16.10.54'],
       'datacenter'       => 'stn',
       'log_level'        => 'INFO',
       'node_name'        => $facts['fqdn'],
